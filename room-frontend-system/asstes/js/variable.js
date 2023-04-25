0dc057fb774e3097c84b6fffb4d4f72f5d948d6b
Pradip Chavda <87759455+chavdapradip48@users.noreply.github.com>
2023-04-26 00:29:06 +0530

    Update variable.js
    backend server  url changed

--- room-frontend-system/asstes/js/variable.js
+++ room-frontend-system/asstes/js/variable.js
@@ -1,3 +1,3 @@
 //const backendServerUrl = 'https://roommates-782r.onrender.com';
 //
-const backendServerUrl="https://room-backend.onrender.com";
+const backendServerUrl="https://room-backend-service.onrender.com";
