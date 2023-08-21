267ee5196dca6e1519ae488600203c4d9a9f510d
Pradip Chavda <87759455+chavdapradip48@users.noreply.github.com>
2023-08-21 17:03:22 +0530

    Update variable.js

--- room-frontend-system/asstes/js/variable.js
+++ room-frontend-system/asstes/js/variable.js
@@ -2,8 +2,8 @@
 // const frontendUrl = "http://localhost:5500";
 const notificationDurationInHour = 1;
 //const backendServerUrl = 'http://localhost:4565';
-const backendServerUrl="https://theroom-backend.onrender.com";
-const frontendUrl = "https://theroom.onrender.com"
+//const backendServerUrl="https://theroom-backend.onrender.com";
+//const frontendUrl = "https://theroom.onrender.com"
 
-//const backendServerUrl="http://ec2-13-49-67-84.eu-north-1.compute.amazonaws.com:7777";
-//const frontendUrl = "http://ec2-13-49-67-84.eu-north-1.compute.amazonaws.com"
+const backendServerUrl="http://ec2-43-205-240-194.ap-south-1.compute.amazonaws.com:7777";
+const frontendUrl = "http://ec2-13-49-67-84.eu-north-1.compute.amazonaws.com"
