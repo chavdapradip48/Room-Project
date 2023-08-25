0a78f87d3e0aa2ff8f86a1bb9ff9443588edcd8a
Pradip Chavda <87759455+chavdapradip48@users.noreply.github.com>
2023-08-25 13:42:33 +0530

    new instance url added

--- room-frontend-system/asstes/js/variable.js
+++ room-frontend-system/asstes/js/variable.js
@@ -2,8 +2,9 @@
 // const frontendUrl = "http://localhost:5500";
 const notificationDurationInHour = 1;
 //const backendServerUrl = 'http://localhost:4565';
-const backendServerUrl="https://theroom-backend.onrender.com";
-const frontendUrl = "https://theroom.onrender.com"
+// New Room instance url
+const backendServerUrl="http://ec2-16-171-249-239.eu-north-1.compute.amazonaws.com:7777";
+const frontendUrl = "http://ec2-16-171-249-239.eu-north-1.compute.amazonaws.com"
 
 //const backendServerUrl="https://ec2-13-232-105-103.ap-south-1.compute.amazonaws.com:7777";
 //const frontendUrl = "http://ec2-13-49-67-84.eu-north-1.compute.amazonaws.com"
