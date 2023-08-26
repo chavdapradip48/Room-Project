9d8a20d6fe169c8843d7bfc40568e1f8a162e371
Pradip Chavda <87759455+chavdapradip48@users.noreply.github.com>
2023-08-26 10:22:53 +0530

    dynamic urls in  variable.js

--- room-frontend-system/asstes/js/variable.js
+++ room-frontend-system/asstes/js/variable.js
@@ -1,10 +1,7 @@
-// const backendServerUrl = 'http://localhost:4564';
-// const frontendUrl = "http://localhost:5500";
 const notificationDurationInHour = 1;
-//const backendServerUrl = 'http://localhost:4565';
-// New Room instance url
-const backendServerUrl="http://ec2-16-171-249-239.eu-north-1.compute.amazonaws.com:7777";
-const frontendUrl = "http://ec2-16-171-249-239.eu-north-1.compute.amazonaws.com"
+
+const frontendUrl = window.location.origin;
+const backendServerUrl= frontendUrl+":7777";
 
 //const backendServerUrl="https://ec2-13-232-105-103.ap-south-1.compute.amazonaws.com:7777";
 //const frontendUrl = "http://ec2-13-49-67-84.eu-north-1.compute.amazonaws.com"
